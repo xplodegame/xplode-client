fae484c5f06b2158f02edd9d806548fc7f4627b7
notlelouch <notlelouchvb@gmail.com>
2024-12-17 16:58:51 +0530

    a bit of refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -15,34 +15,6 @@
 // //   </StrictMode>,
 // // )
 
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-// import { ClerkProvider } from '@clerk/clerk-react'
-
-
-// // Import your Publishable Key
-// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-// if (!PUBLISHABLE_KEY) {
-//   throw new Error('Add your Clerk Publishable Key to the .env.local file')
-// }
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     {/* <Router>
-//        <Routes>
-//          <Route path="/" element={<App />} />
-//          <Route path="/home" element={<Home />} />
-//        </Routes>
-//     </Router> */}
-//     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-//       <App />
-//     </ClerkProvider>
-//   </React.StrictMode>,
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
