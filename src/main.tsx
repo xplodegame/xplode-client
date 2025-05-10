8e85ef8085bea928e530d8f535979ddd8c15e117
Aditya Pandey <adityapandey@Adityas-MacBook-Air.local>
2025-05-11 03:01:59 +0530

    privy change

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ if (!PRIVVY_APP_ID) {
   throw new Error('Missing Privvy App ID')
 }
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <PrivyProvider appId={PRIVVY_APP_ID}> */}
@@ -18,10 +19,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       appId={PRIVVY_APP_ID}
       config={{
         appearance: {
-          theme: 'dark',
-          // Use 'solana-only' or 'ethereum-and-solana'
-          walletChainType: 'solana-only',
-          walletList: ['backpack', 'phantom']
+            theme: 'dark',
+            showWalletLoginFirst: true,
+          // logo: 'https://ibb.co/1YDLXsFX', 
+          // landingHeader: 'Xplode', 
+          // loginMessage: 'Welcome Gaurdian!', 
+            walletChainType: 'solana-only'
         },
         externalWallets: {
           solana: {connectors: solanaConnectors}
@@ -40,35 +43,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </PrivyProvider>
   </React.StrictMode>
 )
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     {/* <PrivyProvider appId={PRIVVY_APP_ID}> */}
-//     <PrivyProvider
-//       appId={PRIVVY_APP_ID}
-//       config={{
-//         appearance: {
-//           theme: 'dark',
-//           showWalletLoginFirst: true,
-//           // logo: 'https://ibb.co/1YDLXsFX', 
-//           // landingHeader: 'Xplode', 
-//           // loginMessage: 'Welcome Gaurdian!', 
-//           walletChainType: 'ethereum-and-solana'
-//         },
-//         externalWallets: {
-//           solana: {connectors: solanaConnectors}
-//         },
-//         // embeddedWallets: { 
-//         //   ethereum: { 
-//         //     createOnLogin: 'users-without-wallets', // defaults to 'off'
-//         //   }, 
-//         //   solana: { 
-//         //     createOnLogin: 'users-without-wallets', // defaults to 'off'
-//         //   }, 
-//         // }, 
-//       }}
-//     >
-//       <MainApp />
-//     </PrivyProvider>
-//   </React.StrictMode>
-// )
